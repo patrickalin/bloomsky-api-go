cf223c36694405093c68ccaf803a75759bb6a7cf
Alin <admin@alin.be>
2019-06-10 15:43:08 +0200

    add git clone

--- command/torch.sh
+++ command/torch.sh
@@ -1,5 +1,5 @@
 echo go-torch
 go get github.com/uber/go-torch
-# git clone git@github.com:brendangregg/FlameGraph.git
+git clone git@github.com:brendangregg/FlameGraph.git
 go-torch --binaryname bloomsky-api-go.test -b prof.cpu
 open -a "google chrome" torch.svg 
