e8afa596c997164c6f56dd225cb609353b4c27fd
Alin <admin@alin.be>
2019-06-10 17:38:04 +0200

    var not used

--- example/example.go
+++ example/example.go
@@ -6,23 +6,22 @@ import (
 	bloomskyStructure "github.com/patrickalin/bloomsky-api-go"
 )
 
-const testFile1 = "testcase/test1.json"
-
 func main() {
 	fmt.Println("Example")
 	// No url, no tocken, mock, no log
 	mybloomskyExample := bloomskyStructure.New("", "", true, nil)
+
 	// Refresh Data
 	mybloomskyExample.Refresh()
+
 	// use Getter
 	fmt.Println("DeviceId from mock file :> ", mybloomskyExample.GetDeviceID())
+
 	// or GetStructure
 	mybloomskyExampleStruct := mybloomskyExample.GetBloomskyStruct()
 	fmt.Println("DeviceId from bloomsky API :> ", mybloomskyExampleStruct.DeviceID)
 	fmt.Println("Win direction from Struct bloomsky API :> ", mybloomskyExampleStruct.Storm.WindDirection)
 	fmt.Println("Win direction degree from API bloomsky API :> ", mybloomskyExample.GetWindDirectionDeg())
 	fmt.Println("Win direction from API bloomsky API :> ", mybloomskyExample.GetWindDirection())
-	// Output:
-	// 442C05954A59
-	// 442C05954A59
+
 }
