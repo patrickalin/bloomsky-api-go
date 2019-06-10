d6d610833f9457084b1d31d1203397a58c9eb8b3
Alin <admin@alin.be>
2019-06-10 16:25:21 +0200

    write real example

--- example/example.go
+++ example/example.go
@@ -1,23 +1,24 @@
-package bloomskyStructure_test
+package main
 
 import (
 	"fmt"
 
-	"github.com/patrickalin/bloomsky-api-go"
+	bloomskyStructure "github.com/patrickalin/bloomsky-api-go"
 )
 
 const testFile1 = "testcase/test1.json"
 
-func ExampleNew() {
+func main() {
+	fmt.Println("Example")
 	// No url, no tocken, mock, no log
 	mybloomskyExample := bloomskyStructure.New("", "", true, nil)
 	// Refresh Data
 	mybloomskyExample.Refresh()
 	// use Getter
-	fmt.Println(mybloomskyExample.GetDeviceID())
+	fmt.Println("From mock file :> ", mybloomskyExample.GetDeviceID())
 	// or GetStructure
 	mybloomskyExampleStruct := mybloomskyExample.GetBloomskyStruct()
-	fmt.Println(mybloomskyExampleStruct.DeviceID)
+	fmt.Println("From bloomsky API :> ", mybloomskyExampleStruct.DeviceID)
 	// Output:
 	// 442C05954A59
 	// 442C05954A59
